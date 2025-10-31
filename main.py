ac7b691c029c7a0b4b5a0edd998b536b9fe2ae06
Kekcek <kiseleff2010@yandex.ru>
2025-10-31 14:47:14 +0300

    add folder 0 and 3

--- main.py
+++ main.py
@@ -16,15 +16,20 @@ path_to_dir = input("Введите путь до папки: ")
 number_object = input("Введите число, обозначающий номер объекта: ")
 count_roi = int(input("Введите количество ROI: "))
 name_main_folder = "N_tar-" + number_object + "_lin_AREA"
+
+name_0_folder = "0_N_tar-" + number_object + "_lin_AREA"
 name_1_folder = "1_N_tar-" + number_object + "_ROI"
 name_2_folder = "2_N_tar-" + number_object + "_sl"
+name_3_folder = "3_work_tar-" + number_object
 name_4_folder = "4_N_tar-" + number_object + "_ROI_nz"
 
 creating_root_folders(path_to_dir, name_main_folder)
 path_to_dir = path_to_dir + r"\\" + name_main_folder
 
+creating_root_folders(path_to_dir, name_0_folder)
 creating_root_folders(path_to_dir, name_1_folder)
 creating_root_folders(path_to_dir, name_2_folder)
+creating_root_folders(path_to_dir, name_3_folder)
 creating_root_folders(path_to_dir, name_4_folder)
 
 inside_1_folder = "*_t-" + number_object
